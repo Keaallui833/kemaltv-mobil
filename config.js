f3880c49d83463f5f084395b71f3d9399d42abbe
Keaallui833 <121031258+Keaallui833@users.noreply.github.com>
2026-05-06 07:55:31 +0300

    Clean up config.js by removing comment
    Removed commented line at the top of config.js

--- config.js
+++ config.js
@@ -1,4 +1,3 @@
-// config.js
 const firebaseConfig = {
     apiKey: "AIzaSyC9R9X3FxqgNQ5kWerDmLRSyijbnquoRzg",
     databaseURL: "https://kemaltv-95504-default-rtdb.europe-west1.firebasedatabase.app"
