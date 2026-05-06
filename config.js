f2942a776d6221b099a7f99b21e9a21c777e4362
Keaallui833 <121031258+Keaallui833@users.noreply.github.com>
2026-05-06 07:49:10 +0300

    Add Firebase configuration to config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+// config.js
+const firebaseConfig = {
+    apiKey: "AIzaSyC9R9X3FxqgNQ5kWerDmLRSyijbnquoRzg",
+    databaseURL: "https://kemaltv-95504-default-rtdb.europe-west1.firebasedatabase.app"
+};
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
