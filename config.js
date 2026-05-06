4ea02c1f9f439b76c5a1208f385aa85df3b1a19e
Keaallui833 <121031258+Keaallui833@users.noreply.github.com>
2026-05-06 08:51:08 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,16 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyC9R9X3FxqgNQ5kWerDmLRSyijbnquoRzg",
-    databaseURL: "https://kemaltv-95504-default-rtdb.europe-west1.firebasedatabase.app"
+  apiKey: "AIzaSyB0jn9kAdRiq8SfLuJwodD8ydrmsmX0nEw",
+  authDomain: "kemaltv-e7af9.firebaseapp.com",
+  projectId: "kemaltv-e7af9",
+  storageBucket: "kemaltv-e7af9.firebasestorage.app",
+  messagingSenderId: "582115406161",
+  appId: "1:582115406161:web:b95b2baf91494286886370",
+  measurementId: "G-57Q4QGHF8K",
+  databaseURL: "https://kemaltv-e7af9-default-rtdb.firebaseio.com"
 };
-firebase.initializeApp(firebaseConfig);
+
+// Firebase başlatma
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
 const db = firebase.database();
